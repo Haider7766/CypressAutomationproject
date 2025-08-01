c17a974d3b17cf8c50fd59014e2bb710ffda13e0
Haider Ali <haabbasi626@gmail.com>
2025-08-01 17:58:04 +0500

    new test

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,17 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: 'cypress/reports',
+    overwrite: false,
+    html: true,
+    json: true,
+  },
   e2e: {
     baseUrl: 'https://opensource-demo.orangehrmlive.com',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
-    }
-  }
-})
+      require('cypress-mochawesome-reporter/plugin')(on);
+    },
+  },
+});
