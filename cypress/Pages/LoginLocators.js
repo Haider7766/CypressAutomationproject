968baec3cc7772168ca882e69dd41ace978395b7
Haider Ali <haabbasi626@gmail.com>
2025-08-04 09:57:36 +0500

    create new locator class

---
+++ cypress/Pages/LoginLocators.js
@@ -0,0 +1,6 @@
+export const loginLocators ={
+txtuname:"input[placeholder='Username']",
+  txtpassword: "input[placeholder='Password']",
+  btn: "button[type='submit']",
+  lblmsg:".oxd-text.oxd-text--h6.oxd-topbar-header-breadcrumb-module"
+};
--- cypress/Pages/LoginPges.js
+++ cypress/Pages/LoginPges.js
@@ -1,23 +1,21 @@
+import { loginLocators } from "./LoginLocators";
 class Login {
-  txtuname = "input[placeholder='Username']";
-  txtpassword = "input[placeholder='Password']";
-  btn = "button[type='submit']";
-  lblmsg = ".oxd-text.oxd-text--h6.oxd-topbar-header-breadcrumb-module";
+  
 
   setUserName(username) {
-    cy.get(this.txtuname).type(username);
+    cy.get(loginLocators.txtuname).type(username);
   }
 
   setPassword(password) {
-    cy.get(this.txtpassword).type(password);
+    cy.get(loginLocators.txtpassword).type(password);
   }
 
   clickSubmit() {
-    cy.get(this.btn).click();
+    cy.get(loginLocators.btn).click();
   }
 
   verifyLogin() {
-    cy.get(this.lblmsg).should('have.text', 'Dashboard');
+    cy.get(loginLocators.lblmsg).should('have.text', 'Dashboard');
   }
 }
 
