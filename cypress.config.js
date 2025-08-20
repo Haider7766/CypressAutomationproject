7819e7f1ca286b9a760d0112abf8f824c9d143c0
Haider Ali <haabbasi626@gmail.com>
2025-08-20 10:48:33 +0500

    Add reporter config for multiple reporters

--- cypress.config.js
+++ cypress.config.js
@@ -1,17 +1,12 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
-  reporter: 'cypress-mochawesome-reporter',
+  reporter: 'cypress-multi-reporters',
   reporterOptions: {
-    reportDir: 'cypress/reports',   // final reports folder
-    overwrite: false,
-    html: true,
-    json: true,
-    embeddedScreenshots: true,      // show screenshots inside the report
-    videoOnFailOnly: false          // record + attach video for all tests
-    //  remove assetsDir — reporter automatically handles videos/screenshots
+    configFile: 'reporter-config.json',
   },
   video: true,
+  screenshotOnRunFailure: true,
   e2e: {
     baseUrl: 'https://ampdev.sigmaprocess.net/account/register',
     setupNodeEvents(on, config) {
